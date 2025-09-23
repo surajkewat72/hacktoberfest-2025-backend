a17ecfbca684546479be580029f6cd3e295e3e4f
Alex Smagin <alexsmagin1@gmail.com>
2025-09-23 18:39:36 -0500

    feat: add server file and app js with express and cors imports to start server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-// server.js
+import app from './src/app.js';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-// main application file
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+export default app
+
